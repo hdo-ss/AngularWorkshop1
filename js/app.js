a535365801008218ccbe22296e7de88824e12941
Hanam Do <hdo@takelessons.com>
2014-11-03 19:50:04 +0000

    Angular setup

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var AngularWorkshop = angular.module('AngularWorkshop', []);
+
+AngularWorkshop.controller('WorldSeriesController', function ($scope, $http)
+{
+  $scope.hi = 'Hello World';
+  
+  // Retrieve the data here using $http (hint, check out $http.get)
+  
+  // and then use that data here:
+  $scope.data = { 'YourJSONData': 'Goes Here' };
+});
+
